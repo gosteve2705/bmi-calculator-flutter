bf2c9f9e4b343d580dca51ee7584d9c9fa6fc0ab
gosteve <nomuojamaro@gmail.com>
2020-05-18 09:39:50 +0100

    added a gesture widget to the reusable male and female cards to track when a card is clicked created a update color method to toggle the cards  when they are clicked to depict on and off showing the cards are selected or not

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -5,7 +5,8 @@ import 'reusable_card.dart';
 import 'icon_content.dart';
 
 const bottomContainerHeight = 80.0;
-const reusuableCardColor = Color(0xff1d1e33);
+const activeCardColor = Color(0xff1d1e33);
+const inActiveCardColor = Color(0xff111328);
 const bottomContainerColor = Color(0xffeb1555);
 
 class InputPage extends StatefulWidget {
@@ -14,6 +15,33 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+  Color maleCardColor = inActiveCardColor;
+  Color femaleCardColor = inActiveCardColor;
+
+  // gender tells us what card was clicked if we pass in 1 = male gender if we pass  2 = female gender
+  void updateCardColor(int gender) {
+    //male card pressed
+    if (gender == 1) {
+      if (maleCardColor == inActiveCardColor) {
+        maleCardColor = activeCardColor;
+        femaleCardColor = inActiveCardColor;
+      } else {
+        maleCardColor = inActiveCardColor;
+        femaleCardColor = activeCardColor;
+      }
+    }
+    //female card pressed
+    if (gender == 2) {
+      if (femaleCardColor == inActiveCardColor) {
+        femaleCardColor = activeCardColor;
+        maleCardColor = inActiveCardColor;
+      } else {
+        femaleCardColor = inActiveCardColor;
+        maleCardColor = activeCardColor;
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,30 +54,50 @@ class _InputPageState extends State<InputPage> {
             child: Row(
               children: <Widget>[
                 Expanded(
-                  child: ReusableCard(
-                    colour: reusuableCardColor,
-                    cardChild: IconContent(icon: FontAwesomeIcons.mars,label: 'MALE',),
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        updateCardColor(1);
+                      });
+                    },
+                    child: ReusableCard(
+                      colour: maleCardColor,
+                      cardChild: IconContent(
+                        icon: FontAwesomeIcons.mars,
+                        label: 'MALE',
+                      ),
+                    ),
                   ),
                 ),
                 Expanded(
-                  child: ReusableCard(colour: reusuableCardColor,
-                    cardChild: IconContent(icon: FontAwesomeIcons.venus,label: 'FEMALE',)
-                    ),
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        updateCardColor(2);
+                      });
+                    },
+                    child: ReusableCard(
+                        colour: femaleCardColor,
+                        cardChild: IconContent(
+                          icon: FontAwesomeIcons.venus,
+                          label: 'FEMALE',
+                        )),
+                  ),
                 )
               ],
             ),
           ),
           Expanded(
-            child: ReusableCard(colour: reusuableCardColor),
+            child: ReusableCard(colour: activeCardColor),
           ),
           Expanded(
             child: Row(
               children: <Widget>[
                 Expanded(
-                  child: ReusableCard(colour: reusuableCardColor),
+                  child: ReusableCard(colour: activeCardColor),
                 ),
                 Expanded(
-                  child: ReusableCard(colour: reusuableCardColor),
+                  child: ReusableCard(colour: activeCardColor),
                 )
               ],
             ),
@@ -65,5 +113,3 @@ class _InputPageState extends State<InputPage> {
     );
   }
 }
-
-
